deaa11bd148ad2339f18105668aab2f568c40dfc
kikiAgustin <kikiagustin62@gmail.com>
2021-06-25 09:18:34 +0700

    edit hrd menjadi manajer

--- application/views/form_login_hrd.php
+++ application/views/form_login_hrd.php
@@ -33,7 +33,7 @@
 <body class="hold-transition login-page">
   <div class="login-box">
     <div class="login-logo">
-      <a href="../../index2.html"><b>Login </b>HRD</a>
+      <a href="../../index2.html"><b>Login </b>Manajer</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
